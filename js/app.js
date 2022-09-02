fba232bd9de85b96e2a11394bd197a671ed0ea9f
nobabsarkar <nobabsarkar2016@gmail.com>
2022-09-02 11:04:07 -0700

    second code push

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 
+function addCodeLink() {
+      fetch('https://openapi.programming-hero.com/api/news/categories')
+            .then(res => res.json())
+            .then(data => displayCode(data.data.news_category))
+      
+}
+const displayCode = (data) => {
+      data.forEach(content => {
+            const addList = document.getElementById("add_list");
+            const li = document.createElement('li');
+            li.innerHTML = `
+                   <li class="nav-link ms-4" style="cursor:pointer"> ${content.category_name}, ${content.category_id} </li>
+            `;
+            addList.appendChild(li)
+      })    
+}
+addCodeLink()
+
+
+
+
 
 
 
